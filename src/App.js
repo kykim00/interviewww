55f0e9ad516a3f4f95effb91a67500968d247af2
kykim00 <jeongsaeki@likelion.org>
2022-03-07 13:09:44 +0900

    💄 global style 추가
    ✨ home 컴포넌트 생성
    
    ➕ styled-reset 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import { Form } from "./components/Form";
+import GlobalStyles from "./styles/GlobalStyles";
+import { Home } from "./pages/Home";
 
 function App({ auth }) {
   return (
     <div>
+      <GlobalStyles />
       <Routes>
-        <Route exact path="/" element={<Login auth={auth} />}></Route>
-        <Route path="/contact" element={<Form auth={auth} />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
+        {/* <Route exact path="/" element={<Login auth={auth} />}></Route> */}
+        {/* <Route path="/contact" element={<Form auth={auth} />}></Route> */}
       </Routes>
     </div>
   );
